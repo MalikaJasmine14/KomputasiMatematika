54c5ce2f34a3458bc7a1ca9cf182e1d1147a86bb
Malika Jasmine14 <malika.jn14@gmail.com>
2025-04-10 09:58:57 +0700

    Pratikum02

---
+++ Pratikum02_KomputasiMatematika_MalikaJasmineNazhiif.m
@@ -0,0 +1,17 @@
+clc %untuk mrnghapus data sebelumnya
+clear 
+%untuk komen atau keterangan
+
+%menghitung luas pesergi panjang
+p=10;
+l=20;
+lp=p*l;
+%disp(lp)
+
+lp1 = p+l;
+lp2 = p-l;
+lp3 = p/l;
+
+disp(lp1)
+disp(lp2)
+disp(lp3)
