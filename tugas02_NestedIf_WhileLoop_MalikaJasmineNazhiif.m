6a62367c50bf0bbef36c843a88a5620fc824a618
Malika Jasmine14 <malika.jn14@gmail.com>
2025-04-15 16:33:59 +0700

    tugas Nested if dan while loop

---
+++ tugas02_NestedIf_WhileLoop_MalikaJasmineNazhiif.m
@@ -0,0 +1,20 @@
+% %Nested If
+% ukuran = 60;
+% harga = 250;
+% 
+% if ukuran > 50
+%     if Harga > 150
+%         disp('boneka mahal');
+%     else
+%         disp('boneka murah');
+%     end
+% else
+%     disp('boneka tidak tersedia');
+% end
+
+%While loop
+% x = 1;
+% while x <= 20
+%     disp(x);
+%     x = x + 2;
+% end
